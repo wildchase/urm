379e441701d7a36a29ae1245a488889f884dadad
panly <a@panly.me>
2018-01-27 18:19:11 +0800

    初始化工程

---
+++ urm-common/src/main/java/com/panly/umr/common/HttpUtil.java
@@ -0,0 +1,104 @@
+package com.panly.umr.common;
+
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class HttpUtil {
+
+	
+	private static RequestConfig requestConfig = RequestConfig.custom()  
+			.setConnectionRequestTimeout(5000)  
+	        .setConnectTimeout(15000)  
+	        .setSocketTimeout(5000)  
+	        .build();
+	
+	
+	/**
+	 * 发送http请求
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static String httpGet(String url) {
+		CloseableHttpClient httpClient = null;
+		CloseableHttpResponse response = null;
+		HttpEntity entity = null;
+		String responseContent = null;
+		try {
+			// 创建默认的httpClient实例.
+			httpClient = HttpClients.createDefault();
+			HttpGet httpGet = new HttpGet(url);
+			httpGet.setConfig(requestConfig);
+			// 执行请求
+			response = httpClient.execute(httpGet);
+			entity = response.getEntity();
+			responseContent = EntityUtils.toString(entity, "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 关闭连接,释放资源
+				if (response != null) {
+					response.close();
+				}
+				if (httpClient != null) {
+					httpClient.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return responseContent;
+	}
+	
+	
+	/**
+	 * 发送http请求
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static String httpPost(String url) {
+		CloseableHttpClient httpClient = null;
+		CloseableHttpResponse response = null;
+		HttpEntity entity = null;
+		String responseContent = null;
+		try {
+			// 创建默认的httpClient实例.
+			httpClient = HttpClients.createDefault();
+			HttpPost httpPost = new HttpPost(url);
+			httpPost.setConfig(requestConfig);
+			// 执行请求
+			response = httpClient.execute(httpPost);
+			entity = response.getEntity();
+			responseContent = EntityUtils.toString(entity, "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 关闭连接,释放资源
+				if (response != null) {
+					response.close();
+				}
+				if (httpClient != null) {
+					httpClient.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return responseContent;
+	}
+	
+	
+	
+}
